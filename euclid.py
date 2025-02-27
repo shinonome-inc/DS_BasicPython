9c795ca26c2fe1546f2cc5a4adae39181b95316d
shiho629 <s.wakuda.24.10.1588@4nonome.com>
2025-02-27 23:21:01 +0900

    control_structures

--- euclid.py
+++ euclid.py
@@ -2,3 +2,12 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+def euclid(x,y):
+    while y != 0:
+        x,y = y, int(x) % int(y)
+    return(x)
+
+result = euclid(a,b)
+print('最大公約数：{}'.format(result))
+
+    
--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,16 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+def is_prime(n):
+    if int(n) < 2:
+        return False
+    for i in range(2,int(n)):
+        if int(n) % i == 0:
+            return False
+    return True
+
+for x in [a,b]:
+    if is_prime(x):
+        print('{}は素数'.format(x))
+    else:
+        print('{}は素数ではない'.format(x))
